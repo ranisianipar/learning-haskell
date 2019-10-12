7fe94b9ae3bec6f9b7683d1fffe70cb71782aa98
Rani Lasma Uli <rani.sianipar@gmail.com>
2019-10-12 19:53:13 +0700

    add number 4, solution

--- exercise.hs
+++ exercise.hs
@@ -20,7 +20,11 @@ iter n f x = (f . iter (n - 1) f) x
 coba :: Int -> Int
 coba a = a + 7
 
--- no. 4
---  foldr itu apa???
-mystery xs = foldr (++) [] (map sing xs) where
+-- no. 4: sum of the squares of the natural numbers 1 to n, using map and foldr
+sumOfSquares n = foldr (++) [] (map square [1..n]) where
+    square x = [x*x]
+
+-- Mystery function just put xs element in new array and put the [5,6] (in param) to the right of xs
+-- this works vice versa with the foldl
+mystery xs = foldl (++) [5,6] (map sing xs) where
     sing x = [x]
