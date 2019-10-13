ce2578db1deccf02700cadd7a1c90d1a3b5157bd
Rani Lasma Uli <rani.sianipar@gmail.com>
2019-10-13 15:46:10 +0700

    implement composeList func

--- high-order-function.hs
+++ high-order-function.hs
@@ -22,6 +22,7 @@ coba a = a + 7
 
 -- no. 4: sum of the squares of the natural numbers 1 to n, using map and foldr
 sumOfSquares n = foldr (\p q -> p + q) 0 (map square [1..n])
+sumOfSquares2 n = foldr (+) 0 (map square [1..n])
 square x = x*x
 
 -- Mystery function just put xs element in new array and put the [5,6] (in param) to the right of xs
@@ -31,5 +32,23 @@ mystery xs = foldl (++) [5,6] (map sing xs) where
 
 -- no. 5: polymorphic identity function
 polymorphicId x = x
-
 -- x can be anything, that's why called polymorphic
+
+-- greaterThan10 :: Int -> Bool
+greaterThan10 x = if x > 10 
+    then True
+    else False
+
+-- question: If f is of type Int -> Bool, at what instance of its most general type a ->
+--           a is id used in each case?
+
+-- no. 6: composeList
+-- compose list of function
+composeMe x y = x . y
+composeList (x:xs) = foldl composeMe x xs
+
+-- function will be composed from leftest element, index move to the right (side)
+composeList2 (x:xs) = foldr (\x y -> x . y) x xs
+
+-- test case: `composeList [succ, polymorphicId, square] 1` --> return 2
+-- test case: `composeList2 [succ, polymorphicId, square] 1` --> return 4
