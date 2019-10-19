282630be83b1d5c30b30713c2853a08fede4897f
Rani Lasma Uli <rani.sianipar@gmail.com>
2019-10-19 09:50:29 +0700

    succeed implement mergesort

--- quiz1.hs
+++ quiz1.hs
@@ -2,3 +2,20 @@
 check x y z = if z `mod` x == 0 && z `mod` y == 0 then True else False
 maxs a b xs = filter (check a b) xs
 kpk x y = head (maxs x y [1..(x*y)])
+
+-- no.2 [(x,y) | x <- [1..3], y <- [1..(2*x)]]
+num2 = [(x,y) | x <- [1..3], y <- [1..(2*x)]]
+-- this list will collect tuples of (x,y), which x be the pointer and value of y adjust to the x.
+
+middle xs = (length xs) `div` 2
+mergeSort [] = []
+mergeSort [a] = [a]
+mergeSort xs = merge (mergeSort rightList) (mergeSort leftList)
+    where rightList = take (middle xs) xs
+          leftList = drop (middle xs) xs
+
+merge a [] = a
+merge [] b = b
+merge (a:as) (b:bs)
+    | a < b = a : (merge as (b:bs))
+    | otherwise = b : (merge (a:as) bs)
