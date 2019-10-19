d4b9c0fb378aced5609b7d41bd6a835456e4515b
Rani Lasma Uli <rani.sianipar@gmail.com>
2019-10-19 15:16:30 +0700

    error, fail to implement foldExpr

--- latihan-uts.hs
+++ latihan-uts.hs
@@ -12,11 +12,21 @@ mapExpr f (x:xs) = f x : mapExpr f xs
 -- result = [5, 6]
 
 evaluate :: Expr -> Int
+-- recursive
 evaluate (C x) = x
 -- test: evaluate (C 1 :+ C 2)
 -- result: 3
 evaluate (e1 :+ e2) = evaluate e1 + evaluate e2
-
 evaluate (e1 :- e2) = evaluate e1 - evaluate e2
 evaluate (e1 :* e2) = evaluate e1 * evaluate e2
-evaluate (e1 :/ e2) = evaluate e1 / evaluate e2
+evaluate (e1 :/ e2) = evaluate e1 `div` evaluate e2
+
+-- implement fold
+foldExpr :: (Expr -> Expr -> Int) -> Int -> [Expr] -> Int
+foldExpr f a [] = a
+foldExpr f a (x:xs) = foldExpr f (f a x) xs
+
+evaluate2Add :: Expr -> Expr -> Int
+evaluate2Add x y = evaluate (x :+ y)
+-- evaluate2 (C x) (C y) = evaluate (C x :+ C y)
+
