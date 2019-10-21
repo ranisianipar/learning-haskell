b28927e93ed991e4259cd858e29d3ce649899adf
Rani Lasma Uli <rani.sianipar@gmail.com>
2019-10-21 13:29:09 +0700

    move subst implementation

--- latihan-uts.hs
+++ latihan-uts.hs
@@ -1,8 +1,6 @@
-data Expr = C Int | Expr :+ Expr | Expr :- Expr
-            | Expr :* Expr | Expr :/ Expr
-            | V String
-            | Let String Expr Expr
-        deriving Show
+data Expr = C Float | Expr :+ Expr | Expr :- Expr | Expr :* Expr | Expr :/ Expr 
+            | V String | Let String Expr Expr
+            deriving Show
 
 -- implement map
 mapExpr :: (Expr -> Int) -> [Expr] -> [Int]
@@ -20,6 +18,21 @@ evaluate (e1 :+ e2) = evaluate e1 + evaluate e2
 evaluate (e1 :- e2) = evaluate e1 - evaluate e2
 evaluate (e1 :* e2) = evaluate e1 * evaluate e2
 evaluate (e1 :/ e2) = evaluate e1 `div` evaluate e2
+
+-- subst
+subst :: String -> Expr -> Expr -> Expr
+subst v0 e0 (V v1) = if (v0 == v1) then e0 else (V v1)
+subst _ _ (C c1) = (C c1)
+subst v0 e0 (e1 :+ e2) = subst v0 e0 e1 :+ subst v0 e0 e2
+subst v0 e0 (e1 :- e2) = subst v0 e0 e1 :- subst v0 e0 e2
+subst v0 e0 (e1 :* e2) = subst v0 e0 e1 :* subst v0 e0 e2
+subst v0 e0 (e1 :/ e2) = subst v0 e0 e1 :/ subst v0 e0 e2
+
+-- OLD SUBST
+-- subst v0 e0 (Let v1 e1 e2) = Let v1 e1 (subst v0 e0 e2) -- perlu koreksi ???
+-- koreksi subst
+subst v0 e0 (Let v1 e1 e2) = Let v1 e1 e2
+
 
 -- implement fold
 foldExpr :: (Expr -> Expr -> Int) -> Int -> [Expr] -> Int
